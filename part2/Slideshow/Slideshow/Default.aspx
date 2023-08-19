9ebe486298f760a21b9bd9d9eaad27385a7261c2
dawsonisawesome.com <46724922+dkani421@users.noreply.github.com>
2023-08-19 15:45:53 -0600

    Slideshow with improper sized images

---
+++ part2/Slideshow/Slideshow/Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Slideshow App</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="Content/styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Slideshow App</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Default.aspx">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="About.aspx">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Contact.aspx">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container mt-4">
+            <h1>Welcome to Slideshow App</h1>
+            <div class="slideshow-container">
+                <asp:Image ID="imgPicture" runat="server" CssClass="slide" />
+                <div class="caption">
+                    <asp:Label ID="lblCaption" runat="server"></asp:Label>
+                </div>
+            </div>
+            <div class="controls">
+                <asp:Button ID="btnStartStop" runat="server" Text="Start" OnClick="btnStartStop_Click" />
+                <asp:Button ID="btnPrev" runat="server" Text="Previous" OnClick="btnPrev_Click" />
+                <asp:Button ID="btnNext" runat="server" Text="Next" OnClick="btnNext_Click" />
+                <asp:Button ID="btnToggleMode" runat="server" Text="Sequential Mode" OnClick="btnToggleMode_Click" />
+            </div>
+        </div>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="Scripts/script.js"></script>
+</body>
+</html>
---
+++ part2/Slideshow/Slideshow/Default.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    private List<Picture> LoadPicturesData()
+    {
+        string filePath = Server.MapPath("~/App_Data/PicturesData.txt");
+        var lines = File.ReadLines(filePath);
+
+        var pictures = lines.Select(line =>
+        {
+            var parts = line.Split(';');
+            return new Picture
+            {
+                Name = parts[0],
+                Location = parts[1],
+                Description = parts[2]
+            };
+        }).ToList();
+
+        return pictures;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            var pictures = LoadPicturesData();
+            Session["Pictures"] = pictures;
+            Session["CurrentIndex"] = 0;
+            Session["IsPlaying"] = false;
+            Session["IsRandom"] = false;
+
+            ShowSlide();
+        }
+    }
+
+    private void ShowSlide()
+    {
+        var pictures = (List<Picture>)Session["Pictures"];
+        var currentIndex = (int)Session["CurrentIndex"];
+
+        if (currentIndex >= 0 && currentIndex < pictures.Count)
+        {
+            var currentPicture = pictures[currentIndex];
+            imgPicture.ImageUrl = currentPicture.Location;
+            lblCaption.Text = currentPicture.Description;
+        }
+    }
+
+    protected void btnStartStop_Click(object sender, EventArgs e)
+    {
+        bool isPlaying = (bool)Session["IsPlaying"];
+        Session["IsPlaying"] = !isPlaying;
+
+        btnStartStop.Text = isPlaying ? "Start" : "Stop";
+
+        if ((bool)Session["IsPlaying"])
+        {
+            StartSlideshow();
+        }
+    }
+
+    private void StartSlideshow()
+    {
+        int currentIndex = (int)Session["CurrentIndex"];
+        List<Picture> pictures = (List<Picture>)Session["Pictures"];
+        bool isRandom = (bool)Session["IsRandom"];
+
+        if (!isRandom)
+        {
+            currentIndex = (currentIndex + 1) % pictures.Count;
+        }
+        else
+        {
+            Random random = new Random();
+            currentIndex = random.Next(pictures.Count);
+        }
+
+        Session["CurrentIndex"] = currentIndex;
+        ShowSlide();
+
+        if ((bool)Session["IsPlaying"])
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(), "StartSlideshow", "setTimeout('__doPostBack(\'btnStartStop\',\'\')', 3000);", true);
+        }
+    }
+
+    protected void btnPrev_Click(object sender, EventArgs e)
+    {
+        int currentIndex = (int)Session["CurrentIndex"];
+        List<Picture> pictures = (List<Picture>)Session["Pictures"];
+
+        currentIndex = (currentIndex - 1 + pictures.Count) % pictures.Count;
+
+        Session["CurrentIndex"] = currentIndex;
+        ShowSlide();
+    }
+
+    protected void btnNext_Click(object sender, EventArgs e)
+    {
+        int currentIndex = (int)Session["CurrentIndex"];
+        List<Picture> pictures = (List<Picture>)Session["Pictures"];
+
+        currentIndex = (currentIndex + 1) % pictures.Count;
+
+        Session["CurrentIndex"] = currentIndex;
+        ShowSlide();
+    }
+
+    protected void btnToggleMode_Click(object sender, EventArgs e)
+    {
+        bool isRandom = (bool)Session["IsRandom"];
+        Session["IsRandom"] = !isRandom;
+
+        btnToggleMode.Text = isRandom ? "Sequential Mode" : "Random Mode";
+    }
+}
