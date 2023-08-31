d920d48ca864fe6b1201a06782f21709269787ff
dawsonisawesome.com <46724922+dkani421@users.noreply.github.com>
2023-08-31 17:17:33 -0600

    solved slideshow play issue now onto sequential, styles added for part1 and part2

--- part2/Slideshow/Slideshow/Default.aspx.cs
+++ part2/Slideshow/Slideshow/Default.aspx.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class _Default : Page
 {
-    private List<Picture> LoadPicturesData()
+    private List<Picture> pictures;
+    private int currentIndex = 0;
+    private bool isPlaying = false;
+    private bool isRandom = false;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadPicturesData();
+            Session["Pictures"] = pictures;
+
+            // Initialize session variables
+            Session["CurrentIndex"] = 0;
+            Session["IsPlaying"] = false;
+            Session["IsRandom"] = false;
+        }
+        else
+        {
+            pictures = (List<Picture>)Session["Pictures"];
+            currentIndex = (int)Session["CurrentIndex"];
+            isPlaying = (bool)Session["IsPlaying"];
+            isRandom = (bool)Session["IsRandom"];
+        }
+
+        ShowSlide();
+    }
+
+    private void LoadPicturesData()
     {
         string filePath = Server.MapPath("~/App_Data/PicturesData.txt");
         var lines = File.ReadLines(filePath);
 
-        var pictures = lines.Select(line =>
+        pictures = lines.Select(line =>
         {
             var parts = line.Split(';');
             return new Picture
@@ -22,58 +49,61 @@ public partial class _Default : Page
                 Description = parts[2]
             };
         }).ToList();
-
-        return pictures;
-    }
-
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        if (!IsPostBack)
-        {
-            var pictures = LoadPicturesData();
-            Session["Pictures"] = pictures;
-            Session["CurrentIndex"] = 0;
-            Session["IsPlaying"] = false;
-            Session["IsRandom"] = false;
-
-            ShowSlide();
-        }
     }
 
     private void ShowSlide()
     {
-        var pictures = (List<Picture>)Session["Pictures"];
-        var currentIndex = (int)Session["CurrentIndex"];
-
-        if (currentIndex >= 0 && currentIndex < pictures.Count)
-        {
-            var currentPicture = pictures[currentIndex];
-            imgPicture.ImageUrl = currentPicture.Location;
-            lblCaption.Text = currentPicture.Description;
-        }
+        var currentPicture = pictures[currentIndex];
+        imgPicture.ImageUrl = currentPicture.Location;
+        lblCaption.Text = currentPicture.Description;
     }
 
     protected void btnStartStop_Click(object sender, EventArgs e)
     {
-        bool isPlaying = (bool)Session["IsPlaying"];
-        Session["IsPlaying"] = !isPlaying;
+        isPlaying = !isPlaying;
+        Session["IsPlaying"] = isPlaying;
 
-        btnStartStop.Text = isPlaying ? "Start" : "Stop";
+        btnStartStop.Text = isPlaying ? "Stop" : "Start";
 
-        if ((bool)Session["IsPlaying"])
+        if (isPlaying)
         {
-
-            // Insert a debug break here
-            //System.Diagnostics.Debugger.Break();
-            StartSlideshow();
+            timer.Enabled = true;
+            ShowSlide();
+        }
+        else
+        {
+            timer.Enabled = false;
         }
     }
 
-    private void StartSlideshow()
+    protected void btnPrev_Click(object sender, EventArgs e)
     {
-        int currentIndex = (int)Session["CurrentIndex"];
-        List<Picture> pictures = (List<Picture>)Session["Pictures"];
-        bool isRandom = (bool)Session["IsRandom"];
+        currentIndex = (currentIndex - 1 + pictures.Count) % pictures.Count;
+        Session["CurrentIndex"] = currentIndex;
+        ShowSlide();
+    }
+
+    protected void btnNext_Click(object sender, EventArgs e)
+    {
+        currentIndex = (currentIndex + 1) % pictures.Count;
+        Session["CurrentIndex"] = currentIndex;
+        ShowSlide();
+    }
+
+    protected void btnToggleMode_Click(object sender, EventArgs e)
+    {
+        isRandom = !isRandom;
+        Session["IsRandom"] = isRandom;
+
+        btnToggleMode.Text = isRandom ? "Sequential Mode" : "Random Mode";
+    }
+
+    protected void timer_Tick(object sender, EventArgs e)
+    {
+        if (!isPlaying)
+        {
+            return;
+        }
 
         if (!isRandom)
         {
@@ -87,40 +117,5 @@ public partial class _Default : Page
 
         Session["CurrentIndex"] = currentIndex;
         ShowSlide();
-
-        if ((bool)Session["IsPlaying"])
-        {
-            ScriptManager.RegisterStartupScript(this, GetType(), "StartSlideshow", "setTimeout('__doPostBack(\'btnStartStop\',\'\')', 3000);", true);
-        }
-    }
-
-    protected void btnPrev_Click(object sender, EventArgs e)
-    {
-        int currentIndex = (int)Session["CurrentIndex"];
-        List<Picture> pictures = (List<Picture>)Session["Pictures"];
-
-        currentIndex = (currentIndex - 1 + pictures.Count) % pictures.Count;
-
-        Session["CurrentIndex"] = currentIndex;
-        ShowSlide();
-    }
-
-    protected void btnNext_Click(object sender, EventArgs e)
-    {
-        int currentIndex = (int)Session["CurrentIndex"];
-        List<Picture> pictures = (List<Picture>)Session["Pictures"];
-
-        currentIndex = (currentIndex + 1) % pictures.Count;
-
-        Session["CurrentIndex"] = currentIndex;
-        ShowSlide();
-    }
-
-    protected void btnToggleMode_Click(object sender, EventArgs e)
-    {
-        bool isRandom = (bool)Session["IsRandom"];
-        Session["IsRandom"] = !isRandom;
-
-        btnToggleMode.Text = isRandom ? "Sequential Mode" : "Random Mode";
     }
 }
--- part2/Slideshow/Slideshow/Default.aspx.designer.cs
+++ part2/Slideshow/Slideshow/Default.aspx.designer.cs
@@ -13,15 +13,6 @@ public partial class _Default
 {
 
     /// <summary>
-    /// form1 control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-    /// <summary>
     /// imgPicture control.
     /// </summary>
     /// <remarks>
@@ -74,4 +65,22 @@ public partial class _Default
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
     protected global::System.Web.UI.WebControls.Button btnToggleMode;
+
+    /// <summary>
+    /// ScriptManager1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+    /// <summary>
+    /// timer control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.Timer timer;
 }
